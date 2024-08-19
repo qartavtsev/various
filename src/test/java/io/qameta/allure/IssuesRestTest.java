1bd60f8f9fc73e8fde8bf2e5fc86a7c1c94899ab
Alexander Kartavtsev <112622680+qartavtsev@users.noreply.github.com>
2024-08-19 15:39:13 +0300

    Update IssuesRestTest.java

--- src/test/java/io/qameta/allure/IssuesRestTest.java
+++ src/test/java/io/qameta/allure/IssuesRestTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @Feature("Issues")
+@Layer("API Tests")
 
 public class IssuesRestTest {
 
